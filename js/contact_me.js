d426a282aaed9c19538a58f9c330975e0cc56ae8
zhoumy <zhoumy@xueqiu.com>
2019-11-13 13:50:14 +0800

    update

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,5 +1,12 @@
 $(function() {
-
+    AV.init({
+        appId: "PmpqD8uKDHcPsGpWm8MqrDwS-gzGzoHsz",
+        appKey: "iXDr4R6MfPjLW49MoeIrlIhV",
+        serverURLs: "https://api.monobenzone.cn"
+    });
+    var Comment = AV.Object.extend('Comment');
+    // 构建对象
+    var Comment = new Todo();
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
@@ -12,45 +19,24 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
-            $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
-                data: {
-                    name: name,
-                    phone: phone,
-                    email: email,
-                    message: message
-                },
-                cache: false,
-                success: function() {
-                    // Success message
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+            Comment.set('name', name);
+            Comment.set('email', email);
+            Comment.set('phone', phone);
+            Comment.set('message', message);
+            // 将对象保存到云端
+            Comment.save().then(function (res) {
+                // Success message
+                $('#success').html("<div class='alert alert-success'>");
+                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                $('#success > .alert-success')
+                    .append("<strong>你的消息已经成功提交，我们会尽快与您联系。 </strong>");
+                $('#success > .alert-success')
+                    .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-                error: function() {
-                    // Fail message
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
-                },
-            })
+                //clear all fields
+                $('#contactForm').trigger("reset");
+            });
         },
         filter: function() {
             return $(this).is(":visible");
